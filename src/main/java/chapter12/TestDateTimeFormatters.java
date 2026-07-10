409c2a39b6f27929bb1a85120cfc798e3f0f3785
Panagiotis Triantafyllos <triantafyllospg@dias.com.gr>
2026-07-10 16:55:14 +0300

    remove space

--- src/main/java/chapter12/TestDateTimeFormatters.java
+++ src/main/java/chapter12/TestDateTimeFormatters.java
@@ -25,7 +25,7 @@ public class TestDateTimeFormatters {
     System.out.println("Custom DateTimeFormatter:");
     DateTimeFormatter customDate12TimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss a");
     System.out.println("12-hour format: " + nowDateTime.format(customDate12TimeFormatter));
-    DateTimeFormatter customDate24TimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss ");
+    DateTimeFormatter customDate24TimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
     System.out.println("24-hour format: " + nowDateTime.format(customDate24TimeFormatter));
     
     ZonedDateTime zonedDateTime = ZonedDateTime.now();
